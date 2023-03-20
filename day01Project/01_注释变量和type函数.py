f0436ac7dcd35b44fea2483e0d18487325cd6f1b
Connor <xiaobang199806@163.com>
2023-03-20 14:11:37 +0800

    day01 commit

---
+++ day01Project/01_注释变量和type函数.py
@@ -0,0 +1,25 @@
+'''
+print('hello world')
+print('hello world')
+print('hello world')
+'''
+# print('hello python')
+print('hello bigdata')
+
+int1 = 12
+print(int1)
+print(type(int1))
+
+str1 = '1212'
+print(str1)
+print(type(str1))
+
+float1 = 12.123
+print(float1)
+print(type(float1))
+
+bool1 = True
+bool2 = False
+print(bool1, bool2)
+print(type(bool1), type(bool2))
+
