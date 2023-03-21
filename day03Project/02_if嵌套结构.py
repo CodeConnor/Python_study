bcded35560ee2282658d46cb83a9d47df44c0f04
Connor <xiaobang199806@163.com>
2023-03-21 19:11:24 +0800

    âupdate day03_01_02_03_04

--- day03Project/02_if嵌套结构.py
+++ day03Project/02_if嵌套结构.py
@@ -3,3 +3,10 @@
 # 酒精含量大于或等于 80mg/100ml 为醉驾。
 # 编写 Python 程序判断是否为酒后驾车。
 
+proof = int(input('车辆驾驶员的血液酒精含量为：'))
+if proof >= 20:
+    print('酒驾')
+    if proof >= 80:
+        print('醉驾')
+else:
+    print('不构成酒驾')
--- day03Project/03_typical_cases.py
+++ day03Project/03_typical_cases.py
@@ -8,3 +8,5 @@ import random
 # 2. 基于模块中的randint(start, stop)闭区间，规定随机数范围
 randnum = random.randint(0, 2)  # 0, 1, 2
 print(randnum)
+
+player = int(input('请出拳（玩家手工输入石头0、剪刀1、布2）：'))
