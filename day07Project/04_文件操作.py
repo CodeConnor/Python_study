c0da388300bc4e4b9d6e26f2171826cb010a01e4
Connor <xiaobang199806@163.com>
2023-03-30 16:20:30 +0800

    update day07_04-05

--- day07Project/04_文件操作.py
+++ day07Project/04_文件操作.py
@@ -1,2 +1,6 @@
 # 案例：把Python项目目录下的python.txt文件，更名为linux.txt，
-# 休眠20s，刷新后，查看效果，然后对这个文件进行删除操作。
+# 查看效果，然后对这个文件进行删除操作。
+import os
+if os.path.exists('python.txt'):  # 判断文件是否存在，防止不存在时报错
+    os.rename('python.txt', 'linux.txt')
+os.remove('linux.txt')
--- day07Project/05_文件夹操作.py
+++ day07Project/05_文件夹操作.py
@@ -3,3 +3,12 @@
 # ② 创建一个新images文件夹以及test文件夹
 # ③ 获取目录下的所有文件
 # ④ 移除test文件夹
+import os
+os.chdir('static')  # 切换
+print(os.getcwd())  # 查看当前目录
+if not os.path.exists('images'):  # 判断是否存在
+    os.mkdir('images')
+if not os.path.exists('test'):
+    os.mkdir('test')
+print(os.listdir())  # 查看所有文件
+os.rmdir('test')
