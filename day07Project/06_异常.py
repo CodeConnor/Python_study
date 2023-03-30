b3010bcb14b534ffbac7c208ba28db2a7048de61
Connor <xiaobang199806@163.com>
2023-03-30 18:55:58 +0800

    update day07_06-07

--- day07Project/06_异常.py
+++ day07Project/06_异常.py
@@ -8,8 +8,26 @@
 
 # 异常捕获案例，输入数字执行除法
 num = int(input('请输入数字：'))
+try:
+    result = 100 / num
+    print(result)
+except:
+    print('出现异常，执行B计划')
 
 # 输出异常信息
+try:
+    result = 100 / num
+    print(result)
+except Exception as e:
+    print(f'--日志：{e}--')  # 用print代替日志
 
 # 加入else，finally
-
+try:
+    f = open('python[备份].txt', 'r', encoding='utf-8')
+except:
+    f = open('python[备份].txt', 'w', encoding='utf-8')
+else:
+    content = f.readlines()
+    print(content)
+finally:
+    f.close()
--- day07Project/07_time模块.py
+++ day07Project/07_time模块.py
@@ -1,1 +1,11 @@
-# 循环1000000次，每次循环添加一个元素进列表，求程序运行时间
+# 循环10000000次，每次循环添加一个元素进列表，求程序运行时间
+from time import *  # 直接导入time的所有函数
+start = time()  # 直接使用函数名获取当前时间，而不需要time.time()
+# 开始循环
+list1 = []
+for i in range(10000000):
+    list1.append(i)
+
+end = time()
+execution = end - start  # 计算程序执行时间
+print(f'程序执行时间为：{execution}s')
