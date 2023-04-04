ff7b3ae3f8fbb2b90a89ca8a8191938bfc018712
Connor <xiaobang199806@163.com>
2023-04-04 11:23:30 +0800

    update day08_04_01

--- day08Project/01_学生管理系统.py
+++ day08Project/01_学生管理系统.py
@@ -34,15 +34,15 @@ def add_stu():
         if user_cmd == 'q' or user_cmd == 'Q':
             break
         else:
+            global students
             name = input('请输入学生姓名：')
             age = int(input('请输入学生年龄：'))
-            gender = input('请输入学性别：')
+            gender = input('请输入学生性别：')
             # 将信息存入字典和列表
             student = {}
             student['name'] = name
             student['age'] = age
             student['gender'] = gender
-            global students
             students.append(student)
 
 
@@ -67,11 +67,38 @@ def del_stu():
                 print('很抱歉，查询不到该学生！')
 
 
+# 定义alter_stu函数，实现修改学生信息
+def alter_stu():
+    while True:
+        user_cmd = input('--输入任意键继续--\n--输入q或者Q退出--\n:')
+        # 判断退出或继续
+        if user_cmd == 'q' or user_cmd == 'Q':
+            break
+        else:
+            global students
+            alter_name = input('请输入所要修改的学生姓名：')
+            for i in students:
+                if i['name'] == alter_name:
+                    # 提示输入修改信息
+                    name = input('请输入新学生姓名：')
+                    age = int(input('请输入新学生年龄：'))
+                    gender = input('请输入新学生性别：')
+                    # 修改students列表内的字典
+                    i['name'] = name
+                    i['age'] = age
+                    i['gender'] = gender
+                    print(f'【{alter_name}】信息已成功修改！')
+                    break
+            # 遍历完成后，没查询到（没有break）时进行提示
+            else:
+                print('很抱歉，查询不到该学生！')
+
+
 # 系统的调用
 while True:
     menu()
     user_str = input('请按照提示输入数字：')
-    # 判断输入内容是否是纯数字
+    # 判断输入内容是否是数字
     user_num = is_digit(user_str)
 
     if user_num == 1:
@@ -79,7 +106,7 @@ while True:
     elif user_num == 2:
         del_stu()
     elif user_num == 3:
-        pass
+        alter_stu()
     elif user_num == 4:
         pass
     elif user_num == 5:
--- day08Project/04_修改.py
+++ day08Project/04_修改.py
@@ -8,3 +8,21 @@ while True:
     if user_cmd == 'q' or user_cmd == 'Q':
         break
     else:
+        # global students
+        alter_name = input('请输入所要修改的学生姓名：')
+        for i in students:
+            if i['name'] == alter_name:
+                # 提示输入修改信息
+                name = input('请输入学生姓名：')
+                age = int(input('请输入学生年龄：'))
+                gender = input('请输入学性别：')
+                # 修改students列表内的字典
+                i['name'] = name
+                i['age'] = age
+                i['gender'] = gender
+                print(f'【{alter_name}】信息已成功修改！')
+                print(students)
+                break
+        # 遍历完成后，没查询到（没有break）时进行提示
+        else:
+            print('很抱歉，查询不到该学生！')
