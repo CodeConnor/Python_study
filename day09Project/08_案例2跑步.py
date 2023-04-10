c975be4e77c87c4ce168d566112831b3ebcfd00f
Connor <xiaobang199806@163.com>
2023-04-10 15:33:31 +0800

    update eg.lihua'

--- day09Project/08_案例2跑步.py
+++ day09Project/08_案例2跑步.py
@@ -1,29 +1,33 @@
 # 案例2：lihua体重75.0公斤，lihua每次跑步会减掉0.10公斤，lihua每次吃东西体重增加0.20公斤。
 # 输出lihua信息
-class WeightChange(object):
+class Person(object):
     # 属性
-    def __init__(self):
-        self.__name = 'Lihua'
-        self.__kg = 75
+    def __init__(self, name, weight):
+        self.__name = name
+        self.__weight = weight
 
     # 方法
+    # 使用__str__输出信息
+    def __str__(self):
+        return f'姓名：{self.__name}，体重：{self.__weight:.2f}'
+
     # 跑步减重，限制体重的位数
     def run(self):
-        self.__kg -= 0.10
-        print(f'{self.__name}减重0.10kg，现体重{self.__kg:.2f}kg')
+        self.__weight -= 0.10
 
     # 进食增重
     def eat(self):
-        self.__kg += 0.20
-        print(f'{self.__name}增重0.20kg，现体重{self.__kg:.2f}kg')
+        self.__weight += 0.20
+
 
 
 # 实例化对象
-Lihua = WeightChange()
-Lihua.eat()
+Lihua = Person('Lihua', 75)
+print(Lihua)
+
+# 执行方法
 Lihua.run()
+print(Lihua)
 
-
-
-
-
+Lihua.eat()
+print(Lihua)
