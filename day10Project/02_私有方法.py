206b6a2f28ce15732ade1d5d16aae8d1f32526d2
Connor <xiaobang199806@163.com>
2023-04-11 11:00:51 +0800

    add examples

---
+++ day10Project/02_私有方法.py
@@ -0,0 +1,32 @@
+# 模拟使用ATM取款
+# 银行 => ATM取款 => ①插卡②用户验证③输入取款金额④取款⑤打印账单
+
+# 不使用私有方法时
+class ATM1(object):
+    def card(self):
+        print('插卡')
+
+    def id_verify(self):
+        print('用户验证')
+
+    def input(self):
+        print('输入取款金额')
+
+    def draw_money(self):
+        print('取款')
+
+    def bill(self):
+        print('打印账单')
+
+
+p1 = ATM1()
+# 完整取款流程需要调用5个方法
+p1.card()
+p1.id_verify()
+p1.input()
+p1.draw_money()
+p1.bill()
+
+# ======================================================
+# 使用私有方法简化程序
+
