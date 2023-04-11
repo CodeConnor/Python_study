cdd09a2d9f1edb1e534a124f9b90680f7be63f83
Connor <xiaobang199806@163.com>
2023-04-11 11:08:31 +0800

    update methods & add public interfaces

--- day10Project/02_私有方法.py
+++ day10Project/02_私有方法.py
@@ -3,30 +3,37 @@
 
 # 不使用私有方法时
 class ATM1(object):
-    def card(self):
+    def __card(self):
         print('插卡')
 
-    def id_verify(self):
+    def __id_verify(self):
         print('用户验证')
 
-    def input(self):
+    def __input(self):
         print('输入取款金额')
 
-    def draw_money(self):
+    def __draw_money(self):
         print('取款')
 
-    def bill(self):
+    def __bill(self):
         print('打印账单')
+
+    # 添加公共接口，实现取款操作
+    def withdraw(self):
+        self.__card()
+        self.__id_verify()
+        self.__input()
+        self.__draw_money()
+        self.__bill()
 
 
 p1 = ATM1()
-# 完整取款流程需要调用5个方法
-p1.card()
-p1.id_verify()
-p1.input()
-p1.draw_money()
-p1.bill()
-
+# 不使用私有方法时，完整取款流程需要调用5个方法
+# p1.card()
+# p1.id_verify()
+# p1.input()
+# p1.draw_money()
+# p1.bill()
 # ======================================================
 # 使用私有方法简化程序
-
+p1.withdraw()
