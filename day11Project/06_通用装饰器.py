0a0636650a96a2cfc9a8c482b94033cbae8f474c
Connor <xiaobang199806@163.com>
2023-04-18 16:39:49 +0800

    update eg.logging

--- day11Project/06_通用装饰器.py
+++ day11Project/06_通用装饰器.py
@@ -1,10 +1,14 @@
 # 定义一个通用装饰器，用于打印日志信息
-
+def logging(fn):
+    def inner(*args, **kwargs):
+        print('-----日志信息；正在进行计算-----')
+        return fn(*args, **kwargs)
+    return inner
 
 # 源函数
+@logging
 def sub_nums(num1, num2):
     result = num1 - num2
     return result
-
 
 print(f'sub = {sub_nums(20, 10)}')
--- day11Project/07_对装饰器传参.py
+++ day11Project/07_对装饰器传参.py
@@ -1,20 +1,26 @@
 # 分别对sum和sub函数输出不同的日志
-
+def decoration(flag):
+    def logging(fn):
+        def inner(*args, **kwargs):
+            if flag == '+':
+                print('-----日志信息；正在进行加法计算-----')
+            elif flag == '-':
+                print('-----日志信息；正在进行减法计算-----')
+            return fn(*args, **kwargs)
+        return inner
+    return logging
 
 # 源函数
-@logging('+')
+@decoration('+')
 def sum_nums(num1, num2):
     result = num1 + num2
     return result
 
-
 print(f'sum = {sum_nums(20, 10)}')
 
-
-@logging('-')
+@decoration('-')
 def sub_nums(num1, num2):
     result = num1 - num2
     return result
-
 
 print(f'sub = {sub_nums(20, 10)}')
