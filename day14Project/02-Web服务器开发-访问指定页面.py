200b5594cc7f2000049ce361b59e26e3bc90f64e
Connor <xiaobang199806@163.com>
2023-04-27 10:44:28 +0800

    update eg.html

--- day14Project/02-Web服务器开发-访问指定页面.py
+++ day14Project/02-Web服务器开发-访问指定页面.py
@@ -16,19 +16,23 @@ if __name__ == '__main__':
         # 接收客户端数据
         client_data = new_socket.recv(4096)
         if client_data:
-            print(client_data)
+            # 对客户端请求解码
+            client_data = client_data.decode('utf-8')
+            # 切片, 获取页面信息和路径
+            request_data = client_data.split(' ', maxsplit=2)
+            request_path = request_data[1]
+
         # 响应数据到客户端
-    #         with open('html/index.html', 'rb') as f:  # 读取html文件，使用 rb 模式是因为需要读音频视频文件等
-    #             html_data = f.read()
-    #
-    #         # 关键点：将以上数据拼接为HTTP响应报文
-    #         response_line = 'HTTP/1.1 200 OK\r\n'  # 响应行
-    #         response_header = 'Server:PWB/1.1\r\n'  # 响应头
-    #         # 空行 => \r\n => 写在拼接中
-    #         response_body = html_data  # 响应体
-    #         # 拼接，将字符串拼接然后编码为二进制数据，响应体不用编码，因为rb模式读出的数据即为二进制数据
-    #         response_data = (response_line + response_header + '\r\n').encode('utf-8') + response_body
-    #         new_socket.send(response_data)
-    #         # 关闭套接字
-    #         new_socket.close()
-    #
+            with open('html' + request_path, 'rb') as f:  # 读取html文件，使用 rb 模式是因为需要读音频视频文件等
+                html_data = f.read()
+
+            # 关键点：将以上数据拼接为HTTP响应报文
+            response_line = 'HTTP/1.1 200 OK\r\n'  # 响应行
+            response_header = 'Server:PWB/1.1\r\n'  # 响应头
+            # 空行 => \r\n => 写在拼接中
+            response_body = html_data  # 响应体
+            # 拼接，将字符串拼接然后编码为二进制数据，响应体不用编码，因为rb模式读出的数据即为二进制数据
+            response_data = (response_line + response_header + '\r\n').encode('utf-8') + response_body
+            new_socket.send(response_data)
+            # 关闭套接字
+            new_socket.close()
