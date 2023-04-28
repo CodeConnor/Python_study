08a3a3fcea9440f8b3063a62576e7f0a0ca08677
Connor <xiaobang199806@163.com>
2023-04-28 13:05:04 +0800

    update eg.yield

---
+++ day15Project/03-yield生成器.py
@@ -0,0 +1,12 @@
+def generator(n):
+    for i in range(5):
+        print('开始生成数据...')
+        yield i  # 每次执行到yield就会返回一个元素，并将程序暂停于此，下次执行从yield的下一句语句开始
+        print('完成1次数据生成...')
+
+g = generator(5)  # g是一个对象
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
