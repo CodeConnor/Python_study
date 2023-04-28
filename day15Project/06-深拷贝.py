b203d4323fed2d6f8e3d30e26c63acbe480827ba
Connor <xiaobang199806@163.com>
2023-04-28 17:27:22 +0800

    add eg.deepcopy

---
+++ day15Project/06-深拷贝.py
@@ -0,0 +1,16 @@
+# 导入模块
+import copy
+
+list1 = [1, 3, 5, [7, 9]]
+# 拷贝list1
+list2 = copy.deepcopy(list1)
+list1.append(11)
+list1[3].append(9)
+print(list1)
+print(list2)
+# 第一层对象地址不同
+print(id(list1))
+print(id(list2))
+# 第二层对象地址也不同
+print(id(list1[3]))
+print(id(list2[3]))
