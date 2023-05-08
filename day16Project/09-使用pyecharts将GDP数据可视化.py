a36f1812f288e1bc1b4f719c398e7ca1ca2b52ea
Connor <xiaobang199806@163.com>
2023-05-08 18:42:34 +0800

    update eg.gdp

--- day16Project/09-使用pyecharts将GDP数据可视化.py
+++ day16Project/09-使用pyecharts将GDP数据可视化.py
@@ -1,11 +1,15 @@
 # GDP数据可视化格式 => [('美国',100亿),('中国',90亿),('日本',80亿)]
+# 只展示GDP排名前十的国家
 import re
 import requests
+from pyecharts import options as opts
+from pyecharts.charts import Pie
 
 # 存储爬取到的国家的名字
 country_list = []
 # # 存储爬取到的gdp数据
 gdp_list = []
+
 
 def get_gdp():
     # 获取html数据
@@ -24,9 +28,19 @@ def get_gdp():
         if result_gdp:
             gdp_list.append(float(result_gdp.group(1)))  # 将数据存储为float类型
 
-    # 组合列表
-    country_gdp = list(zip(country_list, gdp_list))
-    return country_gdp
+    # 组合列表，只保留前十条数据
+    country_gdp = list(zip(country_list[:10], gdp_list[:10]))
+
+    c = (
+        Pie(init_opts=opts.InitOpts(width='1500px', height='800px'))
+        .add("", country_gdp)
+        .set_global_opts(title_opts=opts.TitleOpts(title="2020 Global GDP Top 10"))
+        # b:横坐标数据，c:纵坐标数据
+        # 百分比形式：{c} => {d}%
+        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {d}%"))
+        .render("pie_2020gdp.html")
+    )
+
 
 if __name__ == '__main__':
-    print(get_gdp())
+    get_gdp()
